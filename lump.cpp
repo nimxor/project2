1c1674093e843129ddd67fd14917a3bfba44b05a
Your Name <you@example.com>
2015-10-28 00:44:05 +0530

    A new file added

---
+++ lump.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define sc(a) scanf("%d",&a)
+
+int main()
+{
+	printf("hello\n");
+	return 0;
+}
+
+/*
+1
+
+4.0
+4.0
+5.0
+1.0
+1.0
+1.0
+1.0
+1.0
+4.0
+5.0
+6.0
+20.0
+21.0
+22.0
+25.0
+26.0
+*/
